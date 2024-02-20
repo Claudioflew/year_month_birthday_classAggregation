a52bec7a557830f8a7c32cf42b3184d46e7d1d22
Koichi Nakata <126118326+Claudioflew@users.noreply.github.com>
2024-02-19 16:42:22 -0800

    Created

--- src/main/java/org/example/LabExercise5.java
+++ src/main/java/org/example/LabExercise5.java
@@ -3,7 +3,7 @@
 package org.example;
 
 public class LabExercise5 {
-    final static int NUM_OF_MONTHS = 12;
+    final static int NUM_OF_MONTHS = 12; // Needs to be static!
     public static void main(String[] args) {
         Year.setNumOfMonths(NUM_OF_MONTHS);
         Year myYear = new Year(2024);
